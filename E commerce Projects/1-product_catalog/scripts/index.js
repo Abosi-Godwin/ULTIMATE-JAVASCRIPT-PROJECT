a6f0ab3cde680ab0c8275e316bfe98d915fd6e0c
Abosi-Godwin <bbnl6060@gmail.com>
2024-06-08 22:21:23 +0100

    updated

--- E commerce Projects/1-product_catalog/scripts/index.js
+++ E commerce Projects/1-product_catalog/scripts/index.js
@@ -17,10 +17,39 @@ const apiCaller = async url => {
 }
 
 let allProducts = [];
+
 const getProducts = async () => {
-  
   allProducts = [...await apiCaller("https://fakestoreapi.com/products")];
+  updateProductsCatalogue(allProducts); 
 }
 getProducts();
 
-console.log(allProducts);
+
+const createProductsElement = productDatas => {
+ return `
+        <div class="pdt">
+        <div class="fave">
+        <ion-icon class="heart" name="heart"></ion-icon>
+        </div>
+        <div class="product-info">
+          <img src="${productDatas.image}" alt="" class="product-img">
+          <p class="category">${productDatas.category}</p>
+          <span class="item-id">${productDatas.id}</span>
+          <h3 class="product-title">${productDatas.title}</h3>
+          <h2 class="price">$${productDatas.price.toFixed(2)}</h2>
+        </div>
+        
+        <div class="cart">
+        <button class="addToCart"> add to cart </dutton>
+        </div>
+        </div>`;
+  console.log(productDatas)
+} 
+
+const updateProductsCatalogue = products => {
+  const productsSection = document.querySelector("products-container");
+  
+ products.forEach(product=>{
+   productsSection.insertAdjacentHTML("beforeend",createProductsElement(product));
+ });
+}
