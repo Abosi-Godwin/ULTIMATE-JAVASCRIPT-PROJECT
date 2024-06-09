fdedcf1a178db5f82bea36b2a516137d34a6e92f
Abosi-Godwin <bbnl6060@gmail.com>
2024-06-09 03:53:53 +0100

    updated

--- E commerce Projects/1-product_catalog/scripts/index.js
+++ E commerce Projects/1-product_catalog/scripts/index.js
@@ -27,29 +27,31 @@ getProducts();
 
 const createProductsElement = productDatas => {
  return `
-        <div class="pdt">
+        <div class="pdt w-full shadow-lg p-3 rounded ">
         <div class="fave">
         <ion-icon class="heart" name="heart"></ion-icon>
         </div>
         <div class="product-info">
           <img src="${productDatas.image}" alt="" class="product-img">
-          <p class="category">${productDatas.category}</p>
-          <span class="item-id">${productDatas.id}</span>
+          <div class="py-4">
+          <p class="category uppercase font-bold">${productDatas.category}</p>
           <h3 class="product-title">${productDatas.title}</h3>
-          <h2 class="price">$${productDatas.price.toFixed(2)}</h2>
+          <h2 class="price font-bold">$${productDatas.price.toFixed(2)}</h2>
+          
+        </div>
         </div>
         
         <div class="cart">
-        <button class="addToCart"> add to cart </dutton>
+        <button class="addToCart uppercase text-md rounded font-bold p-2 bg-sky-500 border-4"> add to cart </dutton>
         </div>
         </div>`;
-  console.log(productDatas)
 } 
 
 const updateProductsCatalogue = products => {
-  const productsSection = document.querySelector("products-container");
   
+  const productsSection = document.querySelector("#products-container");
+ 
  products.forEach(product=>{
-   productsSection.insertAdjacentHTML("beforeend",createProductsElement(product));
+   productsSection.insertAdjacentHTML("beforeend", createProductsElement(product));
  });
 }
